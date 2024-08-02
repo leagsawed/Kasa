a4e98702cb31166899f9128f691e86cf6f82791b
leagsawed <gael.dewas@gmail.com>
2024-08-02 15:54:52 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar_logo">
+        <img src="Kasa_Logo.png" alt="Kasa Logo" />
+      </div>
+      <div>Accueil</div>
+      <div>A propros</div>
+    </nav>
+  );
+}
+
+export default Navbar;
